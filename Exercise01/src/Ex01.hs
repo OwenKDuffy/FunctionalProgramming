1f429c7c1cb15d8a222d1caef907b3106c93227f
Owen <duffyow@tcd.ie>
2018-10-14 21:52:12 +0100

    Store Remotely

--- Exercise01/src/Ex01.hs
+++ Exercise01/src/Ex01.hs
@@ -39,7 +39,9 @@ type EDict = Dict String Double
   -- (2) the expression contains a variable not in the dictionary.
 
 eval :: EDict -> Expr -> Maybe Double
-eval d e = Just 1e-99
+eval d e = case simp d e of
+    Val a -> Just a
+    otherwise -> Nothing
 
 -- Part 2 : Simplifying Expressions -- (57 marks) ------------------------------
 
@@ -62,19 +64,21 @@ simp _ e = e  -- simplest case, Val, needs no special treatment
     -- (2b) if any mention of v in e2 is inside another (Def v .. ..)
 
 simpVar :: EDict -> Id -> Expr
-simpVar d v = (Val 1e-99)
+simpVar d v = case find d v of
+    Nothing -> Var v
+    Just a -> Val a
 
 simpAdd :: EDict -> Expr -> Expr -> Expr
-simpAdd d e1 e2 = (Val 1e-99)
+simpAdd d e1 e2 = Add e1 e2
 
 simpSub :: EDict -> Expr -> Expr -> Expr
-simpSub d e1 e2 = (Val 1e-99)
+simpSub d e1 e2 = Sub e1 e2
 
 simpMul :: EDict -> Expr -> Expr -> Expr
-simpMul d e1 e2 = (Val 1e-99)
+simpMul d e1 e2 = Mul e1 e2
 
 simpDvd :: EDict -> Expr -> Expr -> Expr
-simpDvd d e1 e2 = (Val 1e-99)
+simpDvd d e1 e2 = Dvd e1 e2
 
 simpDef :: EDict -> Id -> Expr -> Expr -> Expr
-simpDef d v e1 e2 = (Val 1e-99)
+simpDef d v e1 e2 = Def v e1 e2
